0a307bc2272a092fdc3951438aa07c9547193247
mic-rwk <michalrywak@gmail.com>
2024-12-11 21:16:26 +0100

    Username specification
    Specify username for every user.

--- pdf_menager.py
+++ pdf_menager.py
@@ -8,17 +8,18 @@ class Pdf_menager:
     headers = {'User-Agent': user_agent}
     url = "https://swfis.pwr.edu.pl/fcp/BGBUKOQtTKlQhbx08SlkTWwVQX2o8DAoHNiwFE1wZDyEPG1gnBVcoFW8SBDRKTxMKRy0SODwBBAEIMQheCFVAORFCHzY/81/public/konsultacje/fakultety_24z.pdf"
     reader : PdfReader
+    path : str = "/Users/" + os.getlogin() + "/Desktop/fakultetZima.pdf"
 
     @staticmethod
     def get_info() -> None:
         r = requests.get(Pdf_menager.url, headers=Pdf_menager.headers, stream = True)
-        with open("/Users/micha/Desktop/fakultetZima.pdf", "wb") as fd:
+        with open(Pdf_menager.path, "wb") as fd:
             fd.write(r.content)
 
 #'/Users/micha/Desktop/fakultetZima.pdf'
     @staticmethod
     def download() -> None:
-        Pdf_menager.reader = PdfReader('/Users/micha/Desktop/fakultetZima.pdf')
+        Pdf_menager.reader = PdfReader(Pdf_menager.path)
 
     @staticmethod
     def convert_to_text() -> str:
@@ -28,8 +29,8 @@ class Pdf_menager:
     
     @staticmethod
     def remove_file() -> None:
-        if os.path.exists("/Users/micha/Desktop/fakultetZima.pdf"):
-            os.remove("/Users/micha/Desktop/fakultetZima.pdf")
+        if os.path.exists(Pdf_menager.path):
+            os.remove(Pdf_menager.path)
         else:
             print("The file does not exist")
     
@@ -38,7 +39,9 @@ def main() -> None:
     Pdf_menager.download()
     text = Pdf_menager.convert_to_text()
     print(text)
+    print('\n')
     Pdf_menager.remove_file()
+    
 
 if __name__ == "__main__":
     main()
