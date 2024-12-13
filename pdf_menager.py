704dbf1f5350b862f24a7d86397ecbacc2d44268
mic-rwk <michalrywak@gmail.com>
2024-12-13 21:47:11 +0100

    Class methods
    Change majority of methods to class methods for accessing class-level
    data.

--- pdf_menager.py
+++ pdf_menager.py
@@ -4,33 +4,34 @@ import re
 import os
 
 class Pdf_menager:
+    
     user_agent = "scrapping_script/1.0"
     headers = {'User-Agent': user_agent}
     url = "https://swfis.pwr.edu.pl/fcp/BGBUKOQtTKlQhbx08SlkTWwVQX2o8DAoHNiwFE1wZDyEPG1gnBVcoFW8SBDRKTxMKRy0SODwBBAEIMQheCFVAORFCHzY/81/public/konsultacje/fakultety_24z.pdf"
     reader : PdfReader
     path : str = "/Users/" + os.getlogin() + "/Desktop/fakultetZima.pdf"
 
-    @staticmethod
-    def get_info() -> None:
-        r = requests.get(Pdf_menager.url, headers=Pdf_menager.headers, stream = True)
-        with open(Pdf_menager.path, "wb") as fd:
+    @classmethod
+    def get_info(cls) -> None:
+        r = requests.get(cls.url, headers=cls.headers, stream = True)
+        with open(cls.path, "wb") as fd:
             fd.write(r.content)
 
 #'/Users/micha/Desktop/fakultetZima.pdf'
-    @staticmethod
-    def download() -> None:
-        Pdf_menager.reader = PdfReader(Pdf_menager.path)
+    @classmethod
+    def download(cls) -> None:
+        cls.reader = PdfReader(cls.path)
 
-    @staticmethod
-    def convert_to_text() -> str:
-        page = Pdf_menager.reader.pages[0]
+    @classmethod
+    def convert_to_text(cls) -> str:
+        page = cls.reader.pages[0]
         text = page.extract_text()
         return text
     
-    @staticmethod
-    def remove_file() -> None:
-        if os.path.exists(Pdf_menager.path):
-            os.remove(Pdf_menager.path)
+    @classmethod
+    def remove_file(cls) -> None:
+        if os.path.exists(cls.path):
+            os.remove(cls.path)
         else:
             print("The file does not exist")
     
