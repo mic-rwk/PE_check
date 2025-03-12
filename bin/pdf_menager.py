94a5405baaa757bb248340aa5b218b0cfc003c47
mic-rwk <michalrywak@gmail.com>
2025-03-12 16:47:11 +0100

    Edit url adress
    It has changed.

--- bin/pdf_menager.py
+++ bin/pdf_menager.py
@@ -8,8 +8,7 @@ class Pdf_menager:
     user_agent = "scrapping_script/1.0"
     headers = {'User-Agent': user_agent}
     urls = [
-        "https://swfis.pwr.edu.pl/fcp/BGBUKOQtTKlQhbx08SlkTWwVQX2o8DAoHNiwFE1wZDyEPG1gnBVcoFW8SBDRKTxMKRy0SODwBBAEIMQheCFVAORFCHzY/81/public/konsultacje/fakultety_24z.pdf",
-        "https://swfis.pwr.edu.pl/fcp/JGBUKOQtTKlQhbx08SlkTWwVQX2o8DAoHNiwFE1xVSXhXFVZpCFghUHcKVigEQUw/81/public/konsultacje/fakultety-sesja_24z.pdf"
+        "https://swfis.pwr.edu.pl/fcp/JGBUKOQtTKlQhbx08SlkTWwVQX2o8DAoHNiwFE1xVSXhXFVZpCFghUHcKVigEQUw/81/public/konsultacje/fakultety_24l.pdf"
     ]
     reader : PdfReader
     path : str = "/Users/" + os.getlogin() + "/Desktop/fakultetZima.pdf"
