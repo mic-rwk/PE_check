69fec21244d46c60b10c20e6fb3262ef89575f02
mic-rwk <michalrywak@gmail.com>
2025-04-10 20:25:44 +0200

    Add basic DB functions

--- db_menager.py
+++ db_menager.py
@@ -5,3 +5,8 @@ class dbMenager:
     @classmethod
     def connect():
         return sqlite3.connect("zajecia.db")
+    
+    @classmethod
+    def create_tables(connection):
+        with connection:
+            connection.execute(CREATE_BUILDING_TABLE)
