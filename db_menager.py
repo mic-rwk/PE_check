f788db5e63bb06360eacf8689bce1b3c96ef8b8d
mic-rwk <michalrywak@gmail.com>
2025-04-10 20:27:31 +0200

    Create class table function

--- db_menager.py
+++ db_menager.py
@@ -3,6 +3,9 @@ import sqlite3
 
 CREATE_BUILDING_TABLE  = "CREATE TABLE Building (Building_ID TEXT PRIMARY KEY, Adress TEXT NOT NULL)"
 
+CREATE_CLASS_TABLE = "CREATE TABLE Class ( Class_ID INTEGER PRIMARY KEY, Lecturer_ID INTEGER NOT NULL" \
+"Start_Time TEXT NOT NULL, End_Time TEXT NOT NULL, Max_Capacity INTEGER NOT NULL, Enrolled_Count INTEGER NOT NULL" \
+"Is_Cancelled TEXT NOT NULL, Subject_ID INTEGER NOT NULL)"
 class dbMenager:
 
     @classmethod
